5e4f818273a142bff5a870d6f110dc940a0aee0d
Sam Bartle <filter@sambartle.co.uk>
2024-09-02 19:37:07 +0100

    Add support for MK14 to latest sn32_develop branch

--- keyboards/magicrefiner/mk14/keymaps/default/keymap.c
+++ keyboards/magicrefiner/mk14/keymaps/default/keymap.c
@@ -1,25 +1,8 @@
-/*
-Copyright 2023 Sam Bartle <https://github.com/sambartle>
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 enum layer_names {
     _BASE,
     _FN,
@@ -42,19 +25,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 QK_CLEAR_EEPROM,   _______,   _______,                               _______,                     _______,   _______,     _______,   RGB_SPI,   RGB_VAD,   RGB_SPD   
       	      ),
 };
-
-void keyboard_post_init_user(void) {
-// Customise the debug level
-  //debug_enable=true;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-// If console is enabled, it will print the matrix position and status of each key pressed
-//  #ifdef CONSOLE_ENABLE
-//    uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
-//  #endif
-  return true;
-}
